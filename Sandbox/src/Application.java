c7a8cd782975ec32959caa0a0bbf1a64494b3656
EvilTerry <Evilterixyoutub@gmail.com>
2024-03-09 22:51:46 +0100

    +
    Template

--- Sandbox/src/Application.java
+++ Sandbox/src/Application.java
@@ -1,31 +1,9 @@
 import nl.saxion.app.SaxionApp;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-
-
 public class Application implements Runnable {
 
-    public static void main(String[] args) {
-        SaxionApp.start(new Application(), 1920, 768);
-    }
+    public static void main(String[] args) { SaxionApp.start(new Application(), 1000, 1000); }
     public void run() {
-        int waysToWin = 0;
 
-        BigInteger time = new BigInteger("61677571");
-        BigInteger distance = new BigInteger("430103613071150");
-
-        for(BigInteger j = new BigInteger("0");
-            j.compareTo(time) < 0;
-            j = j.add(BigInteger.ONE))
-        {
-            if(j.multiply((time.subtract(j))).compareTo(distance) > 0)
-            {
-                waysToWin++;
-            }
-        }
-        SaxionApp.printLine(waysToWin);
     }
 }
